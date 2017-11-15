6081de053d3f95271add3e07484e17a03cc57b06
Piotrek <@>
2017-11-15 19:00:17 +0100

    przeslanie plikow

---
+++ Rozw_6_2/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Rozw_6_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Osoba student1 = new Student("Jan", "Kowalski", 1, 2, 34567);
+            Console.WriteLine(student1.ToString());
+
+
+            Console.ReadKey();
+        }
+    }
+}
